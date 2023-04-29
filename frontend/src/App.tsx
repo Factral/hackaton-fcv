48a5f814a6c807a79b8181c977dd6908c1782bd4
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-04-28 20:38:23 -0500

    Modify index.css and create root vars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 function App (): JSX.Element {
   return (
     <>
       <Routes>
-        <Route />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </>
   )
