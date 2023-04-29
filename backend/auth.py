81af5e28e0bec17d08172040811082288e68280f
Sebastian Garcia <sefegade@gmail.com>
2023-04-28 21:47:48 -0500

    actualizando a api

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,6 @@
 # auth.py
 
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from . import db
@@ -9,49 +9,37 @@ from .user import User
 auth = Blueprint('auth', __name__)
 #login
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
-
 @auth.route('/login', methods=['POST'])
 def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
+    email = request.json['email']
+    password = request.json['password']
+    remember = True if request.json['remember'] else False
 
     user = db.find_one({'email': email})
 
     if not user or not User.validate_login(user['password'], password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))
-
-
-    user_obj = User(str(user["_id"]))
-    print(user_obj.username)
+        return jsonify({'message': 'Por favor revisa tus credenciales'}), 400
+    
+    user_obj = User(str(user["_id"]), user["email"])
 
     login_user(user_obj, remember=remember)
 
-    return redirect(url_for('main.profile'))
+    return jsonify({'message': 'Logged in'})
 
 # signup
-
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
 
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
 
     # check if user already exists
     user = db.find_one({'email': email})
 
     if user: 
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
+        return jsonify({'message': 'User already exists'})
 
     # regiter user in mongodb
     new_user = {
@@ -62,10 +50,10 @@ def signup_post():
 
     db.insert_one(new_user)
 
-    return redirect(url_for('auth.login'))
+    return jsonify({'message': 'El usuario ha sido creado'}), 201
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return jsonify({'message': 'Logged out'}), 200
--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,23 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-import pymongo
 
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('login.html')
+    return jsonify({'message': 'Helpter API'})
 
 @main.route('/profile')
 @login_required
 def profile():
-    print(current_user)
-    print(current_user.username)
-    print(current_user.email)
-    return render_template('profile.html', name=current_user.username)
+    user = current_user
+    user_dict = {
+        'id': user.username,
+        'email': user.email,
+        'name': user.name,
+    }
+
+    return jsonify(user_dict)
