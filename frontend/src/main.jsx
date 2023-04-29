6fef9b8c400658ac0c8779abfae406904cf551bf
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-04-28 22:07:47 -0500

    Replace tsx to jsx YES

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
--- frontend/src/pages/Login.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Login (): JSX.Element {
-  return (
-    <div>Login</div>
-  )
-}
