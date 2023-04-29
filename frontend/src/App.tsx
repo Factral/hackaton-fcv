1a1fde1e1f950c836a649e671c9a16e2ebffe62a
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-04-28 20:15:16 -0500

    add zustand and react touter dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-function App () {
+function App (): JSX.Element {
   return (
     <>
       <Routes>
---
+++ frontend/src/interfaces/User.tsx
@@ -0,0 +1,10 @@
+interface User {
+  id: string
+  name: string
+  email: string
+  password: string
+  role: string
+  dateBrith: string
+}
+
+export default User
