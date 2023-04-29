ebe6a015e4a015cf3c772f1e5db38556427b7385
RABrL <robin_brito@hotmail.com>
2023-04-28 20:49:10 -0500

    Creando el componente navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/navbar/Navbar'
 
 function App (): JSX.Element {
   return (
     <>
+    <Navbar/>
       <Routes>
         <Route />
       </Routes>
